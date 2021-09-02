7686cdce7a7a1eaadc71985f0e329b2fbaacabb8
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-09-02 15:56:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+song_status = "";
 
 function preload()
 {
@@ -31,6 +33,23 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+    
+    song_status = harry_potter_song.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        peter_pan_song.stop();
+
+        if(song_status == false)
+        {
+            harry_potter_song.play();
+            document.getElementById("song_name").innerHTML = "Harry Potter Theme Song";
+        }
+    }
 }
 
 function gotPoses(results)
@@ -38,6 +57,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
